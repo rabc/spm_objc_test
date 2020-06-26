09edecbb0cf63fc269ee8ddb3bf4b75d7f8e38d3
rabc <ricardo.carvalho@adjust.com>
2020-06-26 10:02:42 +0200

    use cSettings

--- Package.swift
+++ Package.swift
@@ -7,22 +7,11 @@ let package = Package(
         .iOS(.v8)
     ],
     products: [
-        .library(name: "MyTestSDK",  targets: ["Core"]),
-        // .library(name: "Sociomantic", targets: ["Sociomantic"]),
-        // .library(name: "Criteo", targets: ["Criteo"]),
-        // .library(name: "Trademob", targets: ["Trademob"]),
-        // .library(name: "WebBridge", targets: ["WebBridge"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .library(name: "MyTestSDK",  targets: ["Core"])
     ],
     targets: [
-        //.target(name: "AdjustSdk", dependencies: ["Core"], path: "AdjustSdk"),
-        .target(name: "Core", path: "MyTest", publicHeadersPath: "MyTestSDK"),
-        // .target(name: "Sociomantic", dependencies: ["Core"], path: "plugin/Sociomantic"),
-        // .target(name: "Criteo", dependencies: ["Core"], path: "plugin/Criteo"),
-        // .target(name: "Trademob", dependencies: ["Core"], path: "plugin/Trademob"),
-        // .target(name: "WebBridge", dependencies: ["Core"], path: "AdjustBridge"),
+        .target(name: "Core", path: "MyTest", publicHeadersPath: "MyTestSDK",
+                cSettings: [.headerSearchPath("MyTest/Internals")]
+        )
     ]
 )
